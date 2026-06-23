2ce3a82c0ad0efaaf627b896149877558ec55a06
Magnus Hagdorn <magnus.hagdorn@charite.de>
2026-06-23 12:38:05 +0200

    expand doc strings

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,27 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    
+    :param data: 2d array containing the inflammation data
+    :returns: the daily mean
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    
+    :param data: 2d array containing the inflammation data
+    :returns: the daily maximum"""
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    
+    :param data: 2d array containing the inflammation data
+    :returns: the daily minimum
+    """
     return np.min(data, axis=0)
 
