3dbe83fa0e01d31014536c158bf10f507a16cdd6
Magnus Hagdorn <magnus.hagdorn@charite.de>
2026-06-24 12:48:05 +0200

    fix style

--- inflammation/models.py
+++ inflammation/models.py
@@ -54,9 +54,9 @@ def patient_normalise(data):
 
     if np.any(data < 0):
         raise ValueError("Inflammation values should be non-negative")
-    max = np.max(data, axis=1)
+    max_data = np.max(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
-        normalised = data / max[:, np.newaxis]
+        normalised = data / max_data[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     normalised[normalised < 0] = 0
     return normalised
