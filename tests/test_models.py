f2bf703a55a5f9976d8c9544025004455323008b
Magnus Hagdorn <magnus.hagdorn@charite.de>
2026-06-24 12:26:20 +0200

    check edge cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -52,7 +52,9 @@ def test_daily_max_string():
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
+        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]]),
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]),
     ])
 def test_patient_normalise(test, expected):
     """Test normalisation works for arrays of one and positive integers.
